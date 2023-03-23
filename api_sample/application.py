d84370e9662f35ba94d229b9a37fae42a9510841
Chris Johnson <chrisjohnson00@hotmail.com>
2023-03-23 10:48:24 -0700

    refactor: drop the `users` grouping in the users api response

--- api_sample/application.py
+++ api_sample/application.py
@@ -7,7 +7,7 @@ bp = Blueprint('app', __name__, url_prefix='/api')
 
 @bp.route('/v1/users')
 def index():
-    return jsonify(users=[{'firstname': 'Sam', 'lastname': 'Ample', 'id': 12345},
+    return jsonify([{'firstname': 'Sam', 'lastname': 'Ample', 'id': 12345},
                           {'firstname': 'Ample', 'lastname': 'Sam', 'id': 54321}])
 
 
